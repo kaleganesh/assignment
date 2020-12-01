3f140fbbe8f75672752183dab7a17bc5dd97f3c1
kaleganesh <40052064+kaleganesh@users.noreply.github.com>
2020-12-01 11:42:02 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assingnment_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            child c = new child();
+            Console.WriteLine(c.add(50, 50));
+
+
+        }
+
+
+    }
+
+    abstract class parent
+    {
+        protected abstract int add(int p, int q);
+    }
+
+    class child : parent
+    {
+        protected override int add(int p, int q)
+        {
+            return p + q;
+        }
+    }
+
+
+
+}
