26165cc4a1aae120f35b1dc380862c53c00d773e
kaleganesh <40052064+kaleganesh@users.noreply.github.com>
2020-12-03 09:42:06 +0530

    Add files via upload

---
+++ event handling/event handling/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace event_handling
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            bankevent b1 = new bankevent();
+
+            b1.netBalance = Convert.ToInt32(textBox1.Text);
+
+            if(b1.netBalance>=100000)
+                {
+
+                b1.overbalance(b1.netBalance);
+
+            }
+            else if(b1.netBalance<=5000)
+            {
+                b1.underbalance();
+            }
+            else
+            {
+                MessageBox.Show("you dont need to pay a Tax");
+            }
+        }
+    }
+}
---
+++ event handling/event handling/bankevent.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace event_handling
+{
+    class bankevent
+    {
+       
+
+            public int netBalance { get; set; }
+        public void overbalance(int amount)
+        {
+            double tax = amount * 0.18;
+            MessageBox.Show("Tax  amount is" + tax);
+        }
+        public void underbalance()
+        {
+            MessageBox.Show("your balance is low please maintain minimum balance up to 5000");
+        }
+
+    }
+}
